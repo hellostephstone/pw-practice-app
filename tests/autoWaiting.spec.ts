57c7810e278300554bd555eb73a1217a067360e7
Steph Stone <hellostephstone@gmail.com>
2025-09-18 18:43:15 -0400

    waiting/timeout test examples

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://uitestingplayground.com/ajax')
+  await page.getByText('Button Triggering AJAX Request').click()
+})
+
+test('auto waiting', async ({ page }) => {
+  const successButton = page.locator('.bg-success')
+
+  await successButton.click()
+
+  await successButton.waitFor({ state: 'attached' })
+  const text = await successButton.allTextContents()
+
+  expect(text).toContain('Data loaded with AJAX get request.')
+
+  await expect(successButton).toHaveText('Data loaded with AJAX get request.', { timeout: 20000 })
+})
+
+test('alternative waits', async ({ page }) => {
+  // const successButton = page.locator('.bg-success') ---- just an example
+
+  // _____ wait for element
+  await page.waitForSelector('.bg-success')
+
+  // _____ wait for particular response
+  // await page.waitForResponse('http://uitestingplayground.com/ajaxdata') ---- just an example
+
+  // _____ wait for network calls to be completed ('NOT RECOMMENDED')
+  await page.waitForLoadState('networkidle')
+
+  // const text = successButton.textContent()
+  // expect(text).toContain('Data loaded with AJAX get request.') ---- just an example
+})
