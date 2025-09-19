655433ea43d285b14db9daaf47e310389eb401b7
Steph Stone <hellostephstone@gmail.com>
2025-09-19 16:25:29 -0400

    additional ui components: theme dropdown changes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@playwright/test'
+import { using } from 'rxjs'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/')
@@ -15,7 +16,74 @@ test.describe('Form Layouts page', () => {
       .locator('nb-card', { hasText: 'Using the Grid' })
       .getByRole('textbox', { name: 'Email' })
 
-      await usingTheGridEmailInput.fill('test@test.com')
+    await usingTheGridEmailInput.fill('test@test.com')
+    await usingTheGridEmailInput.clear()
+    await usingTheGridEmailInput.pressSequentially('test2@test.com', { delay: 500 }) //simulate typing and/or typing slowly
 
+    //generic assertion
+    const inputValue = await usingTheGridEmailInput.inputValue()
+    expect(inputValue).toEqual('test2@test.com')
+
+    //locator assertion
+    await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
   })
+
+  test('radio buttons', async ({ page }) => {
+    const usingTheGridForm = page.locator('nb-card', { hasText: 'Using the Grid' })
+
+    // await usingTheGridForm.getByLabel('Option 1').check({ force: true })
+    await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).check({ force: true })
+    const radioStatus = await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).isChecked()
+    expect(radioStatus).toBeTruthy()
+    await expect(usingTheGridForm.getByRole('radio', { name: 'Option 1' })).toBeChecked
+
+    await usingTheGridForm.getByRole('radio', { name: 'Option 2' }).check({ force: true }) //using force since it is hidden
+    expect(await usingTheGridForm.getByRole('radio', { name: 'Option 1' }).isChecked()).toBeFalsy()
+    expect(await usingTheGridForm.getByRole('radio', { name: 'Option 2' }).isChecked()).toBeTruthy()
+  })
+})
+
+test('checkboxes', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Toastr').click()
+
+  await page.getByRole('checkbox', { name: 'Hide on click' }).uncheck({ force: true })
+  await page.getByRole('checkbox', { name: 'Prevent arising of duplicate toast' }).check({ force: true })
+  await page.getByRole('checkbox', { name: 'Show toast with icon' }).uncheck({ force: true })
+
+  const allBoxes = page.getByRole('checkbox')
+  for (const box of await allBoxes.all()) {
+    await box.check({ force: true })
+    expect(await box.isChecked()).toBeTruthy()
+  }
+})
+
+test('lists and dropdowns', async ({ page }) => {
+  const dropDownMenu = page.locator('ngx-header nb-select')
+  await dropDownMenu.click()
+
+  page.getByRole('list') //when the list has a UL tag
+  page.getByRole('listitem') //when the list has LI tag
+
+  // const optionList = page.getByRole('list').locator('nb-option')
+  const optionList = page.locator('nb-option-list nb-option')
+  await expect(optionList).toHaveText(['Light', 'Dark', 'Cosmic', 'Corporate'])
+  await optionList.filter({ hasText: 'Cosmic' }).click()
+  const header = page.locator('nb-layout-header')
+  await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+  const colors = {
+    Light: 'rgb(255, 255, 255)',
+    Dark: 'rgb(34, 43, 69)',
+    Cosmic: 'rgb(50, 50, 89)',
+    Corporate: 'rgb(255, 255, 255)',
+  }
+
+  await dropDownMenu.click()
+  for (const color in colors) {
+    await optionList.filter({ hasText: color }).click()
+    await expect(header).toHaveCSS('background-color', colors[color])
+    if(color != "Corporate")
+      await dropDownMenu.click()
+  }
 })
