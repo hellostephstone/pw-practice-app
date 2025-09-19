16abd69b2bddf04040c770e8f567d6cc8f2fa63b
Steph Stone <hellostephstone@gmail.com>
2025-09-19 17:44:24 -0400

    tooltip, dialog box, and web table examples

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -83,7 +83,53 @@ test('lists and dropdowns', async ({ page }) => {
   for (const color in colors) {
     await optionList.filter({ hasText: color }).click()
     await expect(header).toHaveCSS('background-color', colors[color])
-    if(color != "Corporate")
-      await dropDownMenu.click()
+    if (color != 'Corporate') await dropDownMenu.click()
   }
+})
+
+//freeze browser for macOS fn + F8
+test('tooltips', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Tooltip').click()
+
+  const toolTipCard = page.locator('nb-card', { hasText: 'Tooltip Placements' })
+  await toolTipCard.getByRole('button', { name: 'Top' }).hover()
+
+  page.getByRole('tooltip') //if you have a role tooltip created
+  const tooltip = await page.locator('nb-tooltip').textContent()
+  expect(tooltip).toEqual('This is a tooltip')
+})
+
+test('dialog box', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+
+  page.on('dialog', (dialog) => {
+    expect(dialog.message()).toEqual('Are you sure you want to delete?')
+    dialog.accept()
+  })
+
+  await page.getByRole('table').locator('tr', { hasText: 'mdo@gmail.com' }).locator('.nb-trash').click()
+  await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+})
+
+test('web tables', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+
+  //get the row by any text in this row
+  const targetRow = page.getByRole('row', { name: 'twitter@outlook.com' })
+  await targetRow.locator('.nb-edit').click()
+  await page.locator('input-editor').getByPlaceholder('Age').clear()
+  await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+  await page.locator('.nb-checkmark').click()
+
+  //get the row based on the value in the specific column
+  await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+  const targetRowById = page.getByRole('row', { name: '11' }).filter({ has: page.locator('td').nth(1).getByText('11') })
+  await targetRowById.locator('.nb-edit').click()
+  await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+  await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+  await page.locator('.nb-checkmark').click()
+  await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
 })
