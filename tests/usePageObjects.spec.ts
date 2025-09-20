80de4a02dab1fb4b00ef8c0313ae57fc3009cf78
Steph Stone <hellostephstone@gmail.com>
2025-09-20 19:57:29 -0400

    example screenshots and video results

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -23,8 +23,10 @@ test("parameterized methods", async ({ page }) => {
   await pm.navigateTo().formsLayoutsPage()
   await pm.onFormsLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "Welcome1", "Option 1")
   await pm.onFormsLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
-  await page.screenshot({path: 'screenshots/formsLayoutsPage.png'})
-
+  await page.screenshot({ path: "screenshots/formsLayoutsPage.png" })
+  await page.locator("nb-card", { hasText: "Inline form" }).screenshot({ path: "screenshots/inlineForm.png" })
+  const buffer = await page.screenshot()
+  console.log(buffer.toString("base64"))
   await pm.navigateTo().datepickerPage()
   await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(5)
   await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
