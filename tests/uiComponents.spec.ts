23845effdeb3a305713152249eb83463d2a3f3c9
Steph Stone <hellostephstone@gmail.com>
2025-09-21 14:17:48 -0400

    remove .only from a test

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -29,7 +29,7 @@ test.describe.parallel("Form Layouts page @block", () => {
     await expect(usingTheGridEmailInput).toHaveValue("test2@test.com")
   })
 
-  test.only("radio buttons", async ({ page }) => {
+  test("radio buttons", async ({ page }) => {
     const usingTheGridForm = page.locator("nb-card", { hasText: "Using the Grid" })
 
     // await usingTheGridForm.getByLabel('Option 1').check({ force: true })
